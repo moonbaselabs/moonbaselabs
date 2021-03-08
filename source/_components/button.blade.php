bb511ec2c91cf8e1f267f2bbef38ba224ce37d64
Christian Taylor <christianbtaylor@gmail.com>
2021-03-08 14:41:27 -0600

    Update nonprofit landing page

--- source/_components/button.blade.php
+++ source/_components/button.blade.php
@@ -1,10 +1,19 @@
 @props([
   'href',
+  'size' => 'md',
   'color' => 'onyx'
 ])
 
 @php
-$class = 'inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2';
+$class = 'inline-flex items-center justify-center border border-transparent font-medium rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2';
+switch($size) {
+  case 'md':
+  $class .= ' '.'px-4 py-2 text-sm';
+  break;
+  case 'xl':
+  $class .= ' '.'px-6 py-3 text-base';
+  break;
+}
 switch($color) {
   case 'green':
   $class .= ' '.'text-white bg-green-500 hover:bg-green-400 focus:ring-green-500';
