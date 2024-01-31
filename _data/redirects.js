374bd4b758b6b5af33f9bf07bd8a6509557594c9
Christian Taylor <christianbtaylor@gmail.com>
2024-01-31 13:49:54 -0600

    Redirect case studies

---
+++ _data/redirects.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    from: '/case-studies/arc-physical-therapy/',
+    to: '/missions/arc-physical-therapy/',
+  },
+  {
+    from: '/case-studies/axiom-healthcare-services/',
+    to: '/missions/axiom-healthcare-services/',
+  },
+  {
+    from: '/case-studies/essential-ceu-institute/',
+    to: '/missions/essential-ceu-institute/',
+  },
+  {
+    from: '/case-studies/kansas-beats-the-virus/',
+    to: '/missions/kansas-beats-the-virus/',
+  },
+  {
+    from: '/case-studies/kerii/',
+    to: '/missions/kerii/',
+  },
+  {
+    from: '/case-studies/lubrication-engineers.blade/',
+    to: '/missions/lubrication-engineers.blade/',
+  },
+  {
+    from: '/case-studies/safetyplans/',
+    to: '/missions/safetyplans/',
+  },
+  {
+    from: '/case-studies/venturedash/',
+    to: '/missions/venturedash/',
+  },
+  {
+    from: '/case-studies/workready/',
+    to: '/missions/workready/',
+  },
+]
