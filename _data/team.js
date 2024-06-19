47547410134ba834e39dee857fa04907ae28e6eb
Christian Taylor <christianbtaylor@gmail.com>
2024-06-19 12:24:25 -0500

    Update roles

--- _data/team.js
+++ _data/team.js
@@ -2,7 +2,7 @@ export default {
   "Christian Taylor": {
     name: "Christian Taylor",
     image: '/img/about/christian-taylor.jpg',
-    role: 'All things code',
+    role: 'Code & Design',
     description: "A Fine Arts major that's been writing code for over fifteen years, Christian can design your marketing page and database schema all in the same day. A passionate contributor to the music, art, and tech communities of Wichita.",
     socials: {
       Website: "https://imacrayon.com",
@@ -12,7 +12,7 @@ export default {
   "Nic Wentling": {
     name: "Nic Wentling",
     image: '/img/about/nic-wentling.jpg',
-    role: 'Strategy & partnerships',
+    role: 'Code & Business',
     description: "Formerly a Material and Process Engineer at Textron Aviation, Nic loves to roll up his sleeves and dive head-first into the details. A connoisseur of fine video games and documentaries, he’ll take every opportunity to learn something new.",
     socials: {
       LinkedIn: "https://www.linkedin.com/in/nic-wentling/",
